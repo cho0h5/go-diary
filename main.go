9ce947689c11138eeb35562c152859eda0fa5397
Youngho Cho <dominoyh5@gmail.com>
2020-05-21 20:41:56 +0900

    Update postPost

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -18,18 +19,20 @@ type Data struct {
 
 func logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		go func() {
-			file, err := os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-			if err != nil {
-				panic(err)
-			}
-			defer file.Close()
-
-			log.SetOutput(file)
-			log.Println(r.URL.Path)
-		}()
+		go writeLog(r.URL.Path)
 		f(w, r)
 	}
+}
+
+func writeLog(a ...interface{}) {
+	file, err := os.OpenFile("request.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Println(a)
 }
 
 func getPost(w http.ResponseWriter, r *http.Request) {
@@ -39,20 +42,35 @@ func getPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func postPost(w http.ResponseWriter, r *http.Request) {
-	// p := mux.Vars(r)
-	// i := Data{Code: p["code"], Title: "title", Description: "desc"}
-	// json.NewEncoder(w).Encode(i)
-	fmt.Fprint(w, "postPost test")
+	fileData := r.FormValue("article")
+	go func(fileData string) {
+		path := "data/"
+		os.MkdirAll(path, os.ModePerm)
+
+		fileName := time.Now().Format("20060102_150405")
+		file, err := os.Create(path + fileName + ".txt")
+		if err != nil {
+			writeLog(err)
+		}
+		defer file.Close()
+
+		fmt.Println(fileData) //
+		file.WriteString(fileData)
+	}(fileData)
+	// fmt.Fprint(w, "postPost test")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func main() {
-	router := mux.NewRouter()
 
+	router := mux.NewRouter()
 	router.HandleFunc("/post/{code}", logging(getPost)).Methods("GET")
-	router.HandleFunc("/post}", logging(postPost)).Methods("POST")
-	router.Handle("/", http.FileServer(http.Dir("./static")))
+	router.HandleFunc("/post", logging(postPost)).Methods("POST")
+
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
 
 	http.ListenAndServe(":3000", router)
+
 	// err := http.ListenAndServeTLS(":8080", "server.crt", "server.key", router)
 	// if err != nil {
 	// 	log.Fatal("ListenAndServe: ", err)
